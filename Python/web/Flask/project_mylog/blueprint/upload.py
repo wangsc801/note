44699e9f944d1b5473d36b2418656c57481e4574
wangsc801 <wangsc801@163.com>
2022-10-30 21:58:20 +0800

    update

--- Python/web/Flask/project_mylog/blueprint/upload.py
+++ Python/web/Flask/project_mylog/blueprint/upload.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask import Blueprint, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 from pathlib import Path
 from app import myconfig
@@ -11,19 +10,19 @@ upload_page = Blueprint('upload_page', __name__,
 
 
 class AttachmentForm(FlaskForm):
-    attachment = StringField('Your attachment', validators=[DataRequired()])
+    attachment = FileField('Your attachment', validators=[
+                           FileRequired(), FileAllowed(['jpg', 'png', 'mp4', 'm4v'], "media file only")])
 
 
 @upload_page.route('/', methods=('GET', 'POST'))
 def index():
     form = AttachmentForm()
-    if request.method == 'GET':
-        return render_template('upload/index.html', title="upload", form=form)
     if form.validate_on_submit():
         f = form.attachment.data
-        filename = secure_filename(f.filename)
-        upload_filepath=Path.joinpath(myconfig['UPLOAD_ROOT_DIR'], filename)
-        print(f"\n===\n{upload_filepath}\n===\n")
+        secured_filename = secure_filename(f.filename)
+        root_path = Path(myconfig['UPLOAD_ROOT_DIR'])
+        upload_filepath = root_path.joinpath(secured_filename)
+        print(f"\n\n=====\n{secured_filename}\n-------\n{upload_filepath}\n")
         f.save(upload_filepath)
-        # return redirect(url_for('upload_page.index'))
-        return render_template('upload/index.html', title="upload", form=form)
+        return redirect(url_for('upload_page.index'))
+    return render_template('upload/index.html', title="upload", form=form)
